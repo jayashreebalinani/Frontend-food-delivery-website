8c0beae1b547e8771797d36878dc9a7be88e39c0
Jayashree Balineni <115000642+jayashreebalinani@users.noreply.github.com>
2023-12-19 16:11:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 function loginpage()
 {
    // window.location.href = "login.html"
-
+let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
+width=600,height=300,left=100,top=100`;
 
 
 open('/', 'login.html', params);
